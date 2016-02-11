27fce2287a391ad391768ca21e057779f73531ef
anyjava <sht21c@gmail.com>
2016-02-11 23:35:27 +0900

    기본적인 get Customer Controller 작성

--- src/test/java/net/anyjava/demo/controller/CustomerControllerTest.java
+++ src/test/java/net/anyjava/demo/controller/CustomerControllerTest.java
@@ -1,10 +1,47 @@
 package net.anyjava.demo.controller;
 
-import static org.junit.Assert.*;
+import net.anyjava.SpringDemoApplication;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-/**
- * Created by anyjava on 2016. 2. 11..
- */
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringDemoApplication.class)
+@WebAppConfiguration
 public class CustomerControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    /**
+     * MockMvc
+     */
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .build();
+    }
+
+    @Test
+    public void testGetCustomer() throws Exception {
+        mockMvc.perform(get("/api/Customers/1"))
+                .andExpect(status().isOk());
+//                .andExpect(content().string(containsString("Hello")));
+
+    }
+
 
 }
