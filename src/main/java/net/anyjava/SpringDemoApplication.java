f6a8a4ce804bdb791295abf20a9f1d3d4a9dc2df
anyjava <sht21c@gmail.com>
2016-02-16 23:41:46 +0900

    캐릭터셋 UTF-8으로 변경

--- src/main/java/net/anyjava/SpringDemoApplication.java
+++ src/main/java/net/anyjava/SpringDemoApplication.java
@@ -1,14 +1,42 @@
 package net.anyjava;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpEncodingProperties;
+import org.springframework.boot.context.web.OrderedCharacterEncodingFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.Ordered;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+
+import java.nio.charset.Charset;
 
 @SpringBootApplication
 @ComponentScan
 public class SpringDemoApplication {
 
+    @Autowired
+    private HttpEncodingProperties httpEncodingProperties;
+
     public static void main(String[] args) {
         SpringApplication.run(SpringDemoApplication.class, args);
     }
+
+
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+    }
+
+    @Bean
+    public OrderedCharacterEncodingFilter characterEncodingFilter() {
+        OrderedCharacterEncodingFilter filter = new OrderedCharacterEncodingFilter();
+        filter.setEncoding(this.httpEncodingProperties.getCharset().name());
+        filter.setForceEncoding(this.httpEncodingProperties.isForce());
+        filter.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return filter;
+    }
+
 }
